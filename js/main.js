afc7c66a2d19a45145ad5758671e674c5a2ab89f
SAGH <42285767+8sagh8@users.noreply.github.com>
2022-07-31 14:59:59 -0400

    added references and css

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@
 
 window.onload = function() {
     let data = book["reference"];
-    let text = "";
+    let text = '<div class="container"> <div class="row">';
 
     for (let i = 0; i < data.length; i++){
-        text += '<div class="card" style="width: 18rem;" id = ' + (i + 1) + '> <div class="card-body">';
-        text += '<h5 class="card-text">' + data[i].topic + '</h5>';
+        text += '<div class="card col-4s contClass" style="width: 18rem;" id = ' + (i + 1) + '> <div class="card-body">';
+        text += '<h5 class="card-text topicClass">' + data[i].topic + '</h5><hr/>';
         text += '<p class="card-text">' + data[i].details + '</p> </div> </div>';
     }
 
-    console.log(text)
+    text += '</div> </div>';
     document.getElementById("displayDiv").innerHTML = text;
 }
 
