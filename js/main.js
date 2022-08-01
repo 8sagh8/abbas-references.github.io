9ced4250ab5f979f747f973e74e5fbf17588b95d
SAGH <42285767+8sagh8@users.noreply.github.com>
2022-07-31 20:40:17 -0400

    left nav

--- js/main.js
+++ js/main.js
@@ -4,24 +4,46 @@ let displayReferenceDiv= document.getElementById("displayReferenceDiv");
 let displayTopicDiv = document.getElementById("displayTopicDiv");
 let displayDiv = document.getElementById("displayDiv");
 let homeButtonID = document.getElementById("homeButtonID");
+let sideNavID = document.getElementById("sideNavID");
 
+function getTopicDict(){
+    let data = book["reference"];
+    let topicDict = {};
 
+    for (let i = 0; i < data.length; i++){
+        if ( topicDict.hasOwnProperty(data[i].topic) == false ){
+            topicDict[data[i].topic] = 0;
+        }
+    }
+    console.log(topicDict);
+
+}
 
 function newLoad () {
     displayReferenceDiv.style.display = "none";
     displayTopicDiv.style.display = "none";
     displayDiv.style.display = 'block';
     let data = book["reference"];
+    let topicDict = {};
     let text = '<div class="container"> <div class="row">';
+    let buttonText = '<div>';
 
     for (let i = 0; i < data.length; i++){
         text += '<div class="card col-4s contClass" style="width: 18rem;" id = ' + (i + 1) + '> <div class="card-body">';
         text += '<h5 class="card-text topicClass">' + data[i].topic + '</h5><hr/>';
         text += '<p class="card-text">' + data[i].details + '</p> </div> </div>';
+
+        if ( topicDict.hasOwnProperty(data[i].topic) == false ){
+            topicDict[data[i].topic] = 0;
+            buttonText += '<button type="button" class="" id = "' + data[i].topic + '"><a href="">' + data[i].topic + '</a></button>';
+        }
     }
 
+    buttonText += '</div>';
     text += '</div> </div>';
+    sideNavID.innerHTML = buttonText;
     displayDiv.innerHTML = text;
+    getTopicDict();
 }
 
 // with every window load, this function is called
